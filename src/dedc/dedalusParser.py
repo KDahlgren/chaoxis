f37cec00cc539b8bd4694c694e4fad65689b75bd
KDahlgren <kdahlgren15@gmail.com>
2016-11-06 23:13:29 -0800

    Updated Dedc tests to work with new parser version.

--- src/dedc/dedalusParser.py
+++ src/dedc/dedalusParser.py
@@ -78,52 +78,57 @@ def parse( dedLine ) :
   fact = Word( alphanums + "_" ) + Word("(") + Word( alphanums + "_" + "," + '"' + "'" ) + Word(")") + Optional(arg)
 
   # ------------------------------------------------------------- #
+  #                         RULES                                 #
 
   # return tuples
   if ";" in dedLine :
 
     # parse RULES
     if ":-" in dedLine :
-      #try :
-      if DEDALUSPARSER_DEBUG :
-        print "dedLine = " + dedLine
-      result = rule.parseString( dedLine )
-      ret    = cleanResult( result )
+      try :
+        if DEDALUSPARSER_DEBUG :
+          print "dedLine = " + dedLine
 
-      if DEDALUSPARSER_DEBUG :
-        print "ret = " + str(ret)
+        result = rule.parseString( dedLine )
+        ret    = cleanResult( result )
 
-      # parse attribute lists
-      parsed = []
-      # parse any stubborn subgoals and fmlas
-      for substr in ret :
-        print "pass2: substr = " + substr
-        if ("(" in substr) and (")" in substr) :
-          ret2 = secondParse.parseString( substr )
-          ret2 = cleanResult( ret2 )
-          parsed.extend( ret2 )
-          print "ret2 = " + str(ret2)
-        else :
-          parsed.append( substr )
+        if DEDALUSPARSER_DEBUG :
+          print "ret = " + str(ret)
 
-      if DEDALUSPARSER_DEBUG :
-        print "parsed = " + str(parsed)
+        # parse attribute lists
+        parsed = []
+        # parse any stubborn subgoals and fmlas
+        for substr in ret :
+          print "pass2: substr = " + substr
+          if ("(" in substr) and (")" in substr) :
+            ret2 = secondParse.parseString( substr )
+            ret2 = cleanResult( ret2 )
+            parsed.extend( ret2 )
+            print "ret2 = " + str(ret2)
+          else :
+            parsed.append( substr )
 
-      # clean up comma-separated attributes
-      finalParsed = []
-      for c in parsed :
-        if c == "," : # keep the commas
-          finalParsed.append( c )
-        elif ("," in c) and (not "(" in c) and (not ")" in c) :
-          finalParsed.extend( c.split(",") )
-        else :
-          finalParsed.append(c)
+        if DEDALUSPARSER_DEBUG :
+          print "parsed = " + str(parsed)
 
-      if DEDALUSPARSER_DEBUG :
-        print "*** finalParsed = " + str(finalParsed)
-      return ("rule", finalParsed)
-      #except :
-      #  sys.exit( "\nERROR: Invalid syntax in rule line : \n      " + dedLine + "\n     Note rule attributes cannot have quotes.\n")
+        # clean up comma-separated attributes
+        finalParsed = []
+        for c in parsed :
+          if c == "," : # keep the commas
+            finalParsed.append( c )
+          elif ("," in c) and (not "(" in c) and (not ")" in c) :
+            finalParsed.extend( c.split(",") )
+          else :
+            finalParsed.append(c)
+
+        if DEDALUSPARSER_DEBUG :
+          print "*** finalParsed = " + str(finalParsed)
+        return ("rule", finalParsed)
+      except :
+        sys.exit( "\nERROR: Invalid syntax in rule line : \n      " + dedLine + "\n     Note rule attributes cannot have quotes.\n")
+
+    # ------------------------------------------------------------- #
+    #                         FACTS                                 #
     else :
       # parse FACTS
       if DEDALUSPARSER_DEBUG :
@@ -214,7 +219,8 @@ if __name__ == "__main__" :
   #parse( 'node("a","b")@1;'.translate(None, string.whitespace) )
 
   keywords = [ "notin" ]
-  line = "timer_state(H, I, T-1)@next :- timer_state(H, I, T), notin timer_cancel(H, I), T > 1;"
+  #line = "timer_state(H, I, T-1)@next :- timer_state(H, I, T), notin timer_cancel(H, I), T > 1;"
+  line = "watch('test', 'test')@1;"
   for k in keywords :
     if k in line :
       line = line.replace( k, "___"+k+"___")
