58087dd952fe8418ed368a91886e0172b65051e0
KDahlgren <kdahlgren15@gmail.com>
2016-12-19 12:53:48 -0800

    Added choice of evaluators to setup file.

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 
+import os, sys
+
 # TODO: place magical installation code here
+
+print "Running pyLDFI setup with : \n" + str(sys.argv)
+
+if "c4" in sys.argv :
+  print "Installing C4 Datalog evaluator ... "
+  os.system( "make" )
+  print "... Done installing C4 Datalog evaluator"
+elif "pyDatalog" in sys.argv :
+  print "Using pyDatalog Datalog evaluator."
+else :
+  print "Using pyDatalog Datalog evaluator."
