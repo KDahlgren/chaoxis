294e73ecfc5053c74c17fdf697b9a1b34c7ef9e9
KDahlgren <kdahlgren15@gmail.com>
2017-10-17 15:39:33 -0700

    added 2pc example and support for != operator.

---
+++ dev_tests/2pc/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+
+cmd="time python ../../src/drivers/pyldfi.py -c 0 -n a,b,c --EOT 4 -f ./2pc.ded --evaluator c4"
+opt_cmd="cmd"
+
+rm ./IR.db
+rm ./tmp.txt
+
+if [ "$1" = "$opt_cmd" ]
+then
+  # run command an do not hide stdout
+  $cmd
+  rm ./IR.db
+else
+  # run the test and hide stdout
+  $cmd > tmp.txt
+
+  # check if test passed (TODO: make more sophisticated)
+  if grep -Fxq "PROGRAM EXITED SUCCESSFULLY" tmp.txt
+  then
+    echo "TEST PASSED"
+    rm tmp.txt
+  else
+    echo "TEST FAILED"
+    echo -e "Please see ./tmp.txt for execution outputs, or run the following command for more info :\n$cmd"
+  fi
+fi
+
+# remove the temp file to clean up directory
+#rm tmp.txt
